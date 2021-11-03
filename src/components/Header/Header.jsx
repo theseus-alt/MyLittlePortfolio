c3eb0ff4752fa491533d14811d2ca6db61cb875f
Ritvik Gupta <ritvikgupta75@gmail.com>
2021-11-03 17:26:12 +0530

    initial comit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="head">
+      <span class="material-icons-round">menu</span>
+      <div className="intro">
+        <h3>Hello world!</h3>
+        <h2>I'm Ritvik</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
